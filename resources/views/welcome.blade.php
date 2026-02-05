66afc89e93c4bc03a8c97ce2082585b824b8715c
zoc22 <zoccheridane@gmail.com>
2026-02-05 14:41:34 +0100

    initialisation du projet

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>GitHubLaravel</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+<body>
+    <div class="bg-gray-100 flex items-center justify-center min-h-screen">
+        <h1 class="text-3xl text-green-100 font-bold underline">
+            Hello world!
+        </h1>
+    </div>
+</body>
+</html>
